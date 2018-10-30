0c0c7c0369fc85c5d1526b6432a79f696ae6daf0
Matan Tsuberi <tsuberim@gmail.com>
2018-10-30 18:19:05 +0200

    improve reputation + add tests, refactor a bit

--- src/mappings/Reputation.ts
+++ src/mappings/Reputation.ts
@@ -1,31 +1,54 @@
 import 'allocator/arena'
 export { allocate_memory }
 
-import { Entity, Address, Value, store, crypto, ByteArray } from '@graphprotocol/graph-ts'
+import { Entity, Address, Value, store, crypto, ByteArray, U256 } from '@graphprotocol/graph-ts'
 
 import { Reputation, Mint, Burn } from '../types/Reputation/Reputation'
-import { concat } from '../utils'
+import { concat, zero256, isZero } from '../utils'
 
-let avatar = Address.fromString('a3f5411cfc9eee0dd108bf0d07433b6dd99037f1')
+function update(contract: Address, owner: Address): void {
+    let rep = Reputation.bind(contract);
+    let ent = new Entity();
+    let id = crypto.keccak256(concat(contract, owner)).toHex();
+    ent.setString('id', id);
+    ent.setAddress('contract', contract);
+    ent.setAddress('address', owner);
+    let balance = rep.balanceOf(owner);
+    ent.setU256('balance', balance);
+
+
+    if (!isZero(balance)) {
+        store.set('ReputationHolder', id, ent);
+    } else {
+        store.remove('ReputationHolder', id);
+    }
+}
 
 export function handleMint(event: Mint): void {
-    let accountId = crypto.keccak256(concat(event.params._to, avatar as ByteArray)).toHex()
+    update(event.address, event.params._to);
 
-    let account = store.get('Account', accountId) as Entity
-    if (account == null) {
-        account = new Entity()
-        account.setAddress('address', event.params._to)
-        account.setString('accountId', accountId)
-        account.setAddress('daoAvatarAddress', avatar)
-        account.setU256('reputation', event.params._amount)
-    }
-    store.set('Account', accountId, account as Entity)
+    let ent = new Entity();
+    // TODO: txHash is not unique
+    ent.setString('id', event.transaction.hash.toHex());
+    ent.setString('txHash', event.transaction.hash.toHex());
+    ent.setAddress('contract', event.address);
+    ent.setAddress('address', event.params._to);
+    ent.setU256('amount', event.params._amount);
 
-    let dao = new Entity()
-    dao.setAddress('reputationAddress', event.address)
-    
-    store.set('DAO', avatar.toHex(), dao as Entity)
+    store.set('ReputationMint', event.transaction.hash.toHex(), ent);
+
 }
 
 export function handleBurn(event: Burn): void {
+    update(event.address, event.params._from);
+
+    let ent = new Entity();
+    // TODO: txHash is not unique
+    ent.setString('id', event.transaction.hash.toHex());
+    ent.setString('txHash', event.transaction.hash.toHex());
+    ent.setAddress('contract', event.address);
+    ent.setAddress('address', event.params._from);
+    ent.setU256('amount', event.params._amount);
+
+    store.set('ReputationBurn', event.transaction.hash.toHex(), ent);
 }

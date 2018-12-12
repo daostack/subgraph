f7c14d856f7b33d6faceee70af886a074f0153b5
benk10 <ben.kaufman10@gmail.com>
2018-12-12 18:05:45 +0200

    Update deployDaoStack.js

--- ops/deployDaoStack.js
+++ ops/deployDaoStack.js
@@ -17,22 +17,6 @@ async function deployDaoStack (options) {
     force: true,
     // filepath to output the migration results
     output: path.resolve(migrationFileLocation),
-    // private key of the account used in migration (overrides the 'mnemonic' option)
-    // privateKey: '0x8d4408014d165ec69d8cc9f091d8f4578ac5564f376f21887e98a6d33a6e3549',
-    // mnemonic used to generate the private key of the account used in migration
-    // mnemonic: myth like bonus scare over problem client lizard pioneer submit female collect
-    // migration parameters
-    // params: {
-    // 	default: {
-    // 		// migration params as defined in the "Migration parameters" section below
-    // 	},
-    // 	private: {
-    // 		// overide defaults on private network
-    // 	},
-    // 	kovan: {
-    // 		// overide defaults on kovan
-    // 	},
-    // }
     ...options
   };
 

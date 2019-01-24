231bfeeb9485e7e29073d70622326123a3939d6d
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-01-24 12:37:57 +0100

    restore linter check on travis

--- test/integration/SubscriptionLoop.spec.ts
+++ test/integration/SubscriptionLoop.spec.ts
@@ -43,7 +43,7 @@ describe('Subscriptions Loop', () => {
 
     let event;
     for (let i = 1 ; i <= 10 ; i++) {
-      console.log('subscription number' , i);
+      // console.log('subscription number' , i);
       let nextWasCalled = false;
 
       const consumer = await subscriptionClient.subscribe(

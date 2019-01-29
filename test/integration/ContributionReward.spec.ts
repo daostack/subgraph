24e0da8fdc984ed0a99b3d8d8e6ef2694da5cf25
Ben Kaufman <ben.kaufman10@gmail.com>
2019-01-29 16:04:32 +0200

    roll back migration, fixes

--- test/integration/ContributionReward.spec.ts
+++ test/integration/ContributionReward.spec.ts
@@ -1,4 +1,4 @@
-import { getContractAddresses, getOptions, getWeb3, sendQuery } from './util';
+import { getContractAddresses, getOptions, getWeb3, sendQuery, padZeros } from './util';
 
 const AbsoluteVote = require('@daostack/arc/build/contracts/AbsoluteVote.json');
 const Avatar = require('@daostack/arc/build/contracts/Avatar.json');
@@ -52,7 +52,7 @@ describe('ContributionReward', () => {
             .deploy({ data: AbsoluteVote.bytecode, arguments: [] })
             .send();
 
-        const setParams = absVote.methods.setParameters(20, 0);
+        const setParams = absVote.methods.setParameters(20, '0x0000000000000000000000000000000000000000');
         const absVoteParamsHash = await setParams.call();
         await setParams.send();
         const crSetParams = contributionReward.methods.setParameters(0, absVoteParamsHash, absVote.options.address);

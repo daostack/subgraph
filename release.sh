8f825a6eacc727563dcb31297eea346780304388
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-02-28 18:58:40 +0100

    add ipfs docker image

--- release.sh
+++ release.sh
@@ -6,6 +6,7 @@ set -e
 # publish docker
 echo "Publishing to dockerhub..."
 docker-compose down -v
+docker-compose build
 docker-compose up -d
 
 # wait a bit for graph-node to come (it will redirect with a 302)
@@ -14,21 +15,30 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 127.0.0.1:8000)" != "200" ]
 
 echo "deploying subgraph"
 npm run deploy
-container_id=$(docker ps  -f "name=postgres" -l -q)
-image_name=daostack/subgraph-postgres
+
 migration_version=$(cat package.json  | jq -r '.devDependencies."@daostack/migration"')
 package_version=$(cat package.json | jq -r '.version')
 image_version=ganache-$migration_version-$package_version
+
+# commit the postgres image
+container_id=$(docker ps  -f "name=postgres" -l -q)
+image_name=daostack/subgraph-postgres
 echo "docker commit $container_id $image_name:$image_version"
 docker commit $container_id $image_name:$image_version
 echo "docker push $image_name:$image_version"
 docker push $image_name:$image_version
 
+# commit the ipfs  image
+container_id=$(docker ps  -f "name=ipfs" -l -q)
+image_name=daostack/subgraph-ipfs
+echo "docker commit $container_id $image_name:$image_version"
+docker commit $container_id $image_name:$image_version
+echo "docker push $image_name:$image_version"
+docker push $image_name:$image_version
+
+
+
 docker-compose down -v
-# publish npm
-#echo "Publishing to npm..."
-#npm login
-#npm publish
 # tag on github
 git tag -a $image_version -m "Release of version $image_name:$image_version"
 git push --tags

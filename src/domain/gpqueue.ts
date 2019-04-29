5d6f5186956a5dc8ab27178f49871c9e4215735c
Ben Kaufman <ben.kaufman10@gmail.com>
2019-04-29 15:27:19 +0300

    Fixed issues with gpqueue

--- src/domain/gpqueue.ts
+++ src/domain/gpqueue.ts
@@ -21,27 +21,8 @@ export function updateThreshold(dao: string,
                                 scheme: string ): void {
   let gpQueue = getGPQueue(organizationId.toHex());
   gpQueue.threshold =  threshold;
-  gpQueue.paramsHash = paramsHash;
-
-  if (gpQueue.votingMachine == null) {
-    let gp = GenesisProtocol.bind(gpAddress);
-    let params = gp.parameters(paramsHash);
-    gpQueue.votingMachine = gpAddress;
-    gpQueue.queuedVoteRequiredPercentage = params.value0; // queuedVoteRequiredPercentage
-    gpQueue.queuedVotePeriodLimit = params.value1; // queuedVotePeriodLimit
-    gpQueue.boostedVotePeriodLimit = params.value2; // boostedVotePeriodLimit
-    gpQueue.preBoostedVotePeriodLimit = params.value3; // preBoostedVotePeriodLimit
-    gpQueue.thresholdConst = params.value4; // thresholdConst
-    gpQueue.limitExponentValue = params.value5; // limitExponentValue
-    gpQueue.quietEndingPeriod = params.value6; // quietEndingPeriod
-    gpQueue.proposingRepReward = params.value7;
-    gpQueue.votersReputationLossRatio = params.value8; // votersReputationLossRatio
-    gpQueue.minimumDaoBounty = params.value9; // minimumDaoBounty
-    gpQueue.daoBountyConst = params.value10; // daoBountyConst
-    gpQueue.activationTime = params.value11; // activationTime
-    gpQueue.voteOnBehalf = params.value12; // voteOnBehalf
-    gpQueue.scheme = scheme;
-  }
+  gpQueue.votingMachine = gpAddress;
+  gpQueue.scheme = scheme;
   gpQueue.dao = dao;
   gpQueue.save();
 }

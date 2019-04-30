bd478848e7713bc98c4bf361615b9601bd2d0a55
Oren Sokolowsky <oren@daostack.io>
2019-04-30 15:39:25 +0300

    indexed only dao with indexed avatar

--- src/mappings/Avatar/mapping.ts
+++ src/mappings/Avatar/mapping.ts
@@ -2,8 +2,8 @@ import 'allocator/arena';
 
 import { Address, BigInt, store } from '@graphprotocol/graph-ts';
 
-// Import event types from the Token contract ABI
-import { Avatar, ReceiveEther, SendEther } from '../../types/Avatar/Avatar';
+// Import event types from the Avatar contract ABI
+import { Avatar, OwnershipTransferred, ReceiveEther, SendEther } from '../../types/Avatar/Avatar';
 
 // Import entity types generated from the GraphQL schema
 import { AvatarContract } from '../../types/schema';
@@ -13,17 +13,10 @@ function handleAvatarBalance(
   value: BigInt,
   received: boolean,
 ): void {
-  let avatarSC = Avatar.bind(address);
 
   let avatar = store.get('AvatarContract', address.toHex()) as AvatarContract;
   if (avatar == null) {
-    avatar = new AvatarContract(address.toHex());
-    avatar.address = address;
-    avatar.name = avatarSC.orgName();
-    avatar.nativeReputation = avatarSC.nativeReputation();
-    avatar.nativeToken = avatarSC.nativeToken();
-    avatar.owner = avatarSC.owner();
-    avatar.balance = BigInt.fromI32(0);
+     return;
   }
 
   if (received) {
@@ -40,4 +33,12 @@ export function handleSendEth(event: SendEther): void {
 }
 export function handleReceiveEth(event: ReceiveEther): void {
   handleAvatarBalance(event.address, event.params._value, true);
+}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let avatar = AvatarContract.load(event.address.toHex());
+  if (avatar == null) {
+      avatar = new AvatarContract(event.address.toHex());
+  }
+  avatar.owner = event.params.newOwner;
+  avatar.save();
 }

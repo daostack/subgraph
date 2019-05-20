ad71de1c2875583aae02ea18be59c417e8ae21c8
Ben Kaufman <ben.kaufman10@gmail.com>
2019-05-20 11:40:11 +0300

    fix

--- test/0.0.1-rc.16/DAOToken.spec.ts
+++ test/0.0.1-rc.16/DAOToken.spec.ts
@@ -89,7 +89,7 @@ describe('DAOToken', () => {
     });
 
     const { tokenTransfers } = await sendQuery(`{
-      tokenTransfers(where:{contract: ${daotoken.options.address.toLowerCase()}}) {
+      tokenTransfers(where: {contract: "${daotoken.options.address.toLowerCase()}"}) {
         txHash
         contract
         from

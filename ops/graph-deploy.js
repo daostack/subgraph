5d1af803dc69dfa027fe151913386314997ffeeb
Jelle <jellegerbrandy@gmail.com>
2019-08-21 11:22:09 +0200

    make subgraph usable as a package, and add some parametrization (#303)
    * make subgraph usable as a package, and add some parametrization
    
    * give opts a default value in setup-env
    
    * more parameters
    
    * fix deployment error
    
    * remove comment
    
    * set version to 0.0.26
    
    * more tweaks to make the package usable as a script
    
    * fix some paths
    
    * more tweaking

--- ops/graph-deploy.js
+++ ops/graph-deploy.js
@@ -1,12 +1,16 @@
 const path = require('path')
-const { runGraphCli, subgraphLocation } = require('./graph-cli.js')
-const { graphNode, ipfsNode, subgraphName } = require('./settings')
+const { runGraphCli, subgraphLocation: defaultSubgraphLocation} = require('./graph-cli.js')
+const { graphNode, ipfsNode, subgraphName: defaultSubgraphName } = require('./settings')
+const cwd = path.resolve(`${__dirname}/..`)
 
-async function deploy (cwd) {
-  if (cwd === undefined) {
-    cwd = path.resolve(`${__dirname}/..`)
+
+async function deploy (opts = {}) {
+  if (!opts.subgraphLocation) {
+    opts.subgraphLocation = defaultSubgraphLocation
   }
-  console.log(`using ${cwd} and ${subgraphLocation}`)
+  if (!opts.subgraphName) {
+    opts.subgraphName = defaultSubgraphName
+  }
   let result
   let msg
 
@@ -15,17 +19,18 @@ async function deploy (cwd) {
     'create',
     '--access-token ""',
     '--node ' + graphNode,
-    subgraphName
+    opts.subgraphName
   ], cwd)
   msg = result[1] + result[2]
-  if (result[0] === 1) {
-    throw Error(`Create failed! ${msg}`)
-  }
+  // if (result[0] === 1) {
+  //   console.error(`Create failed! ${msg}`)
+  // }
   if (msg.toLowerCase().indexOf('error') > 0) {
     if (msg.match(/subgraph already exists/)) {
       // the subgraph was already created before -we're ok
+      console.log('subgraph already exists - deploying a new version')
     } else {
-      throw Error(`Create failed! ${msg}`)
+      console.error(`Create failed! ${msg}`)
     }
   }
 
@@ -34,8 +39,8 @@ async function deploy (cwd) {
     '--access-token "${access_token-""}"',
     '--ipfs ' + ipfsNode,
     '--node ' + graphNode,
-    subgraphName,
-    subgraphLocation
+    opts.subgraphName,
+    opts.subgraphLocation
   ], cwd)
   msg = result[1] + result[2]
   if ((result[0] === 1)|| (result[0] === 127)){

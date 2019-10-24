c33989a112ae32e7d0f8c33f0bd359787df76d92
Oren Sokolowsky <oren@daostack.io>
2019-10-24 16:09:50 +0300

    make it generic :)

--- test/sanity.spec.ts
+++ test/sanity.spec.ts
@@ -1,12 +1,14 @@
 
 describe('sanity', () => {
-
+  const packageJson = require(`@daostack/migration/package.json`);
   it('Sanity', async () => {
    // validate ops/mappings.json include latest arc on private network.
+    const latestArc = packageJson.dependencies['@daostack/arc'];
+
     const mappings = require(`../ops/mappings.json`);
     let latestArcVersionExist = false;
     for (let i = 0; i < mappings.private.mappings.length; i++) {
-        if (mappings.private.mappings[i].arcVersion === '0.0.1-rc.30') {
+        if (mappings.private.mappings[i].arcVersion === latestArc) {
           latestArcVersionExist = true;
         }
     }

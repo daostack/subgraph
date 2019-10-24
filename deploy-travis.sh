af94e62c7470ffac4798838ee284665344391d9d
Ben <ben.kaufman10@gmail.com>
2019-10-24 17:25:24 +0300

    deploy from travis

---
+++ deploy-travis.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+source .env
+
+# set for production
+export graph_node="https://api.thegraph.com/deploy/"
+export ipfs_node="https://api.thegraph.com/ipfs-daostack/"
+
+# deploy mainnet
+export network="mainnet"
+export subgraph="daostack/master"
+npm run deploy
+
+# deploy rinkeby
+export network="rinkeby"
+export subgraph="daostack/master_rinkeby"
+npm run deploy
+
+# Switch to staging key
+export access_token=staging_access_token
+
+# set for production
+export graph_node="https://api.staging.thegraph.com/deploy/"
+export ipfs_node="https://api.staging.thegraph.com/ipfs-daostack/"
+
+# deploy mainnet
+export network="mainnet"
+export subgraph="daostack/master"
+npm run deploy
+
+# deploy rinkeby
+export network="rinkeby"
+export subgraph="daostack/master_rinkeby"
+npm run deploy

c05ba5f137cc4f2efe77cd75310c918c98d5901c
Oren Sokolowsky <oren@daostack.io>
2019-10-24 16:03:02 +0300

    clean

--- test/sanity.spec.ts
+++ test/sanity.spec.ts
@@ -2,14 +2,14 @@
 describe('sanity', () => {
 
   it('Sanity', async () => {
-   //validate mapping include latest arc on private network.
+   // validate ops/mappings.json include latest arc on private network.
     const mappings = require(`../ops/mappings.json`);
-    let versionExist = false;
+    let latestArcVersionExist = false;
     for (let i = 0; i < mappings.private.mappings.length; i++) {
         if (mappings.private.mappings[i].arcVersion === '0.0.1-rc.30') {
-          versionExist = true;
+          latestArcVersionExist = true;
         }
     }
-    expect(versionExist).toEqual(true);
+    expect(latestArcVersionExist).toEqual(true);
   }, 100);
 });

5793bf537d31f18895d4ec0321224b4ba4783882
benk10 <ben@daostack.io>
2019-10-28 14:29:13 +0200

    Deploy travis (#370)
    * deploy from travis
    
    * check on master
    
    * Fix
    
    * Update .travis.yml
    
    * Fix
    
    * change branch
    
    * remove if (for testing)
    
    * fix
    
    * chmod
    
    * fix
    
    * move script
    
    * Revert "move script"
    
    This reverts commit 6737a88be9e96f5bd99799f2d836da9b82b74646.
    
    * add travis wait
    
    * fix ipfs
    
    * only on specific branch + fix
    
    * use master
    
    * only master
    
    * only master

---
+++ deploy-travis.sh
@@ -0,0 +1,31 @@
+if [ ! -z "$access_token" ]; then
+    # set for production
+    export graph_node="https://api.thegraph.com/deploy/"
+    export ipfs_node="https://api.thegraph.com/ipfs-daostack/"
+
+    # deploy mainnet
+    export network="mainnet"
+    export subgraph="daostack/master"
+    npm run deploy
+
+    # deploy rinkeby
+    export network="rinkeby"
+    export subgraph="daostack/master_rinkeby"
+    npm run deploy
+
+    # Switch to staging key
+    export access_token=$staging_access_token
+
+    # set for production
+    export graph_node="https://api.staging.thegraph.com/deploy/"
+
+    # deploy mainnet
+    export network="mainnet"
+    export subgraph="daostack/master"
+    npm run deploy
+
+    # deploy rinkeby
+    export network="rinkeby"
+    export subgraph="daostack/master_rinkeby"
+    npm run deploy
+fi

1fc11a8eeef5aca33a753dbc23b5d16b144ef485
dOrgJelli <jelli@dorg.tech>
2019-11-04 13:56:33 +0100

    updates based on feedback

--- src/mappings/DAOTracker/mapping.ts
+++ src/mappings/DAOTracker/mapping.ts
@@ -10,6 +10,7 @@ import {
   BlacklistedDAO,
   DAOTrackerContract,
   ResetDAO as ResetDAOEntity,
+  UControllerOrganization
 } from '../../types/schema';
 import {
   Avatar_0_0_1_rc_31,
@@ -26,7 +27,7 @@ export function getDAOTrackerContract(address: Address): DAOTrackerContract {
     daoTracker.address = address;
     let daoTrackerSC = DAOTracker.bind(address);
     daoTracker.owner = daoTrackerSC.owner();
-    store.set('DAOTrackerContract', daoTracker.id, daoTracker);
+    daoTracker.save();
   }
   return daoTracker;
 }
@@ -35,25 +36,25 @@ export function handleTrackDAO(event: TrackDAO): void {
   // Ensure the DAOTrackerContract has been added to the store
   getDAOTrackerContract(event.address);
 
-  let _avatar = event.params._avatar;
-  let _controller = event.params._controller;
-  let _reputation = event.params._reputation;
-  let _daoToken = event.params._daoToken;
+  let avatar = event.params._avatar;
+  let controller = event.params._controller;
+  let reputation = event.params._reputation;
+  let daoToken = event.params._daoToken;
 
   // If the avatar already exists, early out
-  if (AvatarContract.load(_avatar.toHex()) != null) {
+  if (AvatarContract.load(avatar.toHex()) != null) {
     return;
   }
 
   // Tell the subgraph to start indexing events from the:
   // Avatar, Controller, DAOToken, and Reputation contracts
-  Avatar_0_0_1_rc_31.create(_avatar);
-  Reputation_0_0_1_rc_31.create(_reputation);
-  DAOToken_0_0_1_rc_31.create(_daoToken);
+  Avatar_0_0_1_rc_31.create(avatar);
+  Reputation_0_0_1_rc_31.create(reputation);
+  DAOToken_0_0_1_rc_31.create(daoToken);
 
   // Track the Controller if it isn't a UController we're already tracking
-  if (store.get('UControllerOrganization', _controller.toHex()) == null) {
-    Controller_0_0_1_rc_31.create(_controller);
+  if (UControllerOrganization.load(controller.toHex()) == null) {
+    Controller_0_0_1_rc_31.create(controller);
   }
 
   // Note, no additional work is needed here because...
@@ -66,28 +67,28 @@ export function handleBlacklistDAO(event: BlacklistDAO): void {
   // Ensure the DAOTrackerContract has been added to the store
   let daoTracker = getDAOTrackerContract(event.address);
 
-  let _avatar = event.params._avatar;
-  let _explanationHash = event.params._explanationHash;
+  let avatar = event.params._avatar;
+  let explanationHash = event.params._explanationHash;
 
   // Add the BlacklistedDAO to the store
-  let blacklistedDAO = new BlacklistedDAO(_avatar.toHex());
-  blacklistedDAO.address = _avatar;
+  let blacklistedDAO = new BlacklistedDAO(avatar.toHex());
+  blacklistedDAO.address = avatar;
   blacklistedDAO.tracker = daoTracker.id;
-  blacklistedDAO.explanationHash = _explanationHash;
+  blacklistedDAO.explanationHash = explanationHash;
 
-  if (!equalStrings(_explanationHash, '')) {
-    let explanation = ipfs.cat('/ipfs/' + _explanationHash);
+  if (!equalStrings(explanationHash, '')) {
+    let explanation = ipfs.cat('/ipfs/' + explanationHash);
 
     if (explanation != null) {
       blacklistedDAO.explanation = explanation.toString();
     }
   }
 
-  store.set('BlacklistedDAO', blacklistedDAO.id, blacklistedDAO);
+  blacklistedDAO.save();
 
   // If the DAO has been previously reset, remove that entity from the store
-  if (store.get('ResetDAO', _avatar.toHex())) {
-    store.remove('ResetDAO', _avatar.toHex());
+  if (ResetDAOEntity.load(avatar.toHex())) {
+    store.remove('ResetDAO', avatar.toHex());
   }
 }
 
@@ -95,26 +96,26 @@ export function handleResetDAO(event: ResetDAO): void {
   // Ensure the DAOTrackerContract has been added to the store
   getDAOTrackerContract(event.address);
 
-  let _avatar = event.params._avatar;
-  let _explanationHash = event.params._explanationHash;
+  let avatar = event.params._avatar;
+  let explanationHash = event.params._explanationHash;
 
   // Remove the BlacklistedDAO from the store
-  if (store.get('BlacklistedDAO', _avatar.toHex())) {
-    store.remove('BlacklistedDAO', _avatar.toHex());
+  if (BlacklistedDAO.load(avatar.toHex())) {
+    store.remove('BlacklistedDAO', avatar.toHex());
   }
 
   // Add the ResetDAO entity to the store
-  let resetDAO = new ResetDAOEntity(_avatar.toHex());
-  resetDAO.address = _avatar;
-  resetDAO.explanationHash = _explanationHash;
+  let resetDAO = new ResetDAOEntity(avatar.toHex());
+  resetDAO.address = avatar;
+  resetDAO.explanationHash = explanationHash;
 
-  if (!equalStrings(_explanationHash, '')) {
-    let explanation = ipfs.cat('/ipfs/' + _explanationHash);
+  if (!equalStrings(explanationHash, '')) {
+    let explanation = ipfs.cat('/ipfs/' + explanationHash);
 
     if (explanation != null) {
       resetDAO.explanation = explanation.toString();
     }
   }
 
-  store.set('ResetDAO', resetDAO.id, resetDAO);
+  resetDAO.save();
 }

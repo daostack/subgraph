ca9f7c1abb2b973cf13efc646be2692082156259
Ben <ben.kaufman10@gmail.com>
2019-12-03 11:09:11 +0200

    Use migration v34

--- ops/generate-daos-subgraph.js
+++ ops/generate-daos-subgraph.js
@@ -38,6 +38,12 @@ function daoYaml(contract, contractAddress, arcVersion) {
       entities,
       abis: (abis || [contract]).map(contractName => {
         let _arcVersion = Number(arcVersion.slice(arcVersion.length-2,arcVersion.length));
+        if ((_arcVersion >= 34) && (contractName === "UGenericScheme")) {
+          return {
+            name: contractName,
+            file: `${__dirname}/../abis/0.0.1-rc.33/UGenericScheme.json`
+          };
+        }
         if ((_arcVersion < 24) && (contractName === "UGenericScheme")) {
           return {name: contractName,
                   file: path.resolve(`./abis/${arcVersion}/GenericScheme.json`)

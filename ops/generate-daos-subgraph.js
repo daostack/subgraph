e6f0de812c9d02d1d092d25080e0aef2f26fdc1b
Ben <ben.kaufman10@gmail.com>
2019-12-04 12:25:47 +0200

    Deprecate UGenericScheme

--- ops/generate-daos-subgraph.js
+++ ops/generate-daos-subgraph.js
@@ -39,10 +39,7 @@ function daoYaml(contract, contractAddress, arcVersion) {
       abis: (abis || [contract]).map(contractName => {
         let _arcVersion = Number(arcVersion.slice(arcVersion.length-2,arcVersion.length));
         if ((_arcVersion >= 34) && (contractName === "UGenericScheme")) {
-          return {
-            name: contractName,
-            file: `${__dirname}/../abis/0.0.1-rc.33/UGenericScheme.json`
-          };
+          throw Error(`Contract UGenericScheme is not supported for Arc version 34+.`)
         }
         if ((_arcVersion < 24) && (contractName === "UGenericScheme")) {
           return {name: contractName,

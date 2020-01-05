692c9264d884ed1a101d2d28ac3854f6100a3d3a
p <paulseidler1@gmail.com>
2020-01-05 20:54:13 +0100

    add generatestring, replace,  generate and append - all in debug mode. To test the individual functions change the signal.data

--- src/domain/signal.ts
+++ src/domain/signal.ts
@@ -1,7 +1,6 @@
 import { Address, BigDecimal, BigInt, ByteArray, Bytes, crypto, ipfs, json, JSONValueKind, store } from '@graphprotocol/graph-ts';
 import { Signal, Proposal } from '../types/schema';
-import { getProposal } from './proposal'
-import { concat, debug } from '../utils';
+import { debug } from '../utils';
 
 export function getSignal(id: string): Signal {
   let sig = store.get('Signal', id) as Signal;
@@ -16,25 +15,57 @@ function saveSignal(signal: Signal): void {
 }
 
 export function writesignal(signalId: string, proposalId: string): void {
-  let newsignal = readProposal(signalId, proposalId)
-  saveSignal(newsignal);
+  readProposal(signalId, proposalId)
 }
 
-export function generatestring(key: string, value: string, signal: Signal ): Signal {
-  const newvar = '{'+ '"' + key + '"' +':' + '"'+ value + '"' + '}';
-  debug(newvar);
+export function generatestring(key: string, value: string, signal: Signal ): void {
+  const newkeypair = '"' + key + '"' +':' + '"'+ value + '"';
+  signal.data = '{"text":"website.jpg","Header":"website.jpg","info":"hello friends"}'
+  let searchstringposition = signal.data.indexOf(key)
   if (signal.data == null){
-    const data = '[' + newvar + ']';
-    signal.data = data;
-    debug(signal.data);
-  }else{
-    debug("This means there was data here");
+    generatenewdict(newkeypair,signal)
   }
-  return signal
+  else if(searchstringposition === -1){
+    appenddict(newkeypair,signal)
+  }
+  else{
+    replaceindict(newkeypair,key,signal)
+  }
 }
 
+function replaceindict(newkeypair: string, key: string, signal: Signal):void{
+  let lenght = signal.data.length;
+  let cutstring = signal.data.slice(1,lenght-1);
+  let stringarray = cutstring.split(',')
+  let finalstring = ""
+  for (let y = 0; y < stringarray.length; ++y) {
+    if (stringarray[y].indexOf(key) === -1){
+      finalstring = finalstring + "," + stringarray[y];
+    }else{
+      finalstring = finalstring + "," + newkeypair;
+      }
+    }
+    signal.data = "{"+finalstring.slice(1)+"}"
+    debug(signal.data);
+    saveSignal(signal);
+  }
 
-export function readProposal(id: string, proposalId: string): Signal {
+function generatenewdict(keypair: string, signal: Signal): void{
+  const data = '{' + keypair + '}';
+  debug("generate");
+  signal.data = data;
+  debug(signal.data);
+  saveSignal(signal);
+}
+
+function appenddict(keypair: string, signal: Signal):void{
+  let signalsubstring = signal.data.substring(0,signal.data.length-1)
+  signal.data = signalsubstring + "," + keypair + "}"
+  debug(signal.data);
+  saveSignal(signal);
+}
+
+export function readProposal(id: string, proposalId: string): void {
   let signal = getSignal(id);
   var key = '';
   var value = '';
@@ -45,7 +76,7 @@ export function readProposal(id: string, proposalId: string): Signal {
 
     let descJson = json.fromBytes(ipfsData as Bytes);
     if (descJson.kind !== JSONValueKind.OBJECT) {
-      return signal;
+      debug("");
     }
     if (descJson.toObject().get('key') != null) {
       key = descJson.toObject().get('key').toString();
@@ -55,6 +86,6 @@ export function readProposal(id: string, proposalId: string): Signal {
     }
 
   }
-  let signalstring = generatestring(key,value,signal)
-  return signalstring;
+  generatestring(key,value,signal)
+
 }

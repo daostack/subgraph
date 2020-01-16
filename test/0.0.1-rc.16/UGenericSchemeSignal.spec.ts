8aadf08c05815122c01d2735d2a39407bd08cec6
p <paulseidler1@gmail.com>
2020-01-16 15:54:08 +0100

    WIP

--- test/0.0.1-rc.16/UGenericSchemeSignal.spec.ts
+++ test/0.0.1-rc.16/UGenericSchemeSignal.spec.ts
@@ -22,7 +22,7 @@ describe('Generic Signal Scheme', () => {
     opts = await getOptions(web3);
   });
 
-  it('generic scheme proposal', async () => {
+  it('generic scheme proposal generate ', async () => {
     const accounts = web3.eth.accounts.wallet;
 
     const genericScheme = new web3.eth.Contract(
@@ -187,37 +187,225 @@ describe('Generic Signal Scheme', () => {
       },
     });
 
-    // const metaq = `{
-    //   signals{
-    //     id
-    //     data
-    //   }
-    // }`
-
-
     const metaq = `{
-      debugs{
+      signals{
         id
-        message
+        data
       }
     }`
 
 
+    // const metaq = `{
+    //   debugs{
+    //     id
+    //     message
+    //   }
+    // }`
+
+
     const metadata = await sendQuery(metaq, 5000);
-    console.log(metadata);
-    //
-    // expect(metadata).toMatchObject({
-    //    signals:[
-    //       {
-    //         data:
-    //           '{"signal": {"Header":"https://de.wikipedia.org/wiki/Wald#/media/Datei:Laurisilva_en_el_Cubo_de_la_Galga.jpg"}}',
-    //         id:
-    //           '0xe7a2c59e134ee81d4035ae6db2254f79308e334f'
-    //       }
-    //     ]
-    // });
+
+    expect(metadata).toMatchObject({
+       signals:[
+          {
+            data:
+              '{"Header":"https://de.wikipedia.org/wiki/Wald#/media/Datei:Laurisilva_en_el_Cubo_de_la_Galga.jpg"}',
+            id:
+              '0xe7a2c59e134ee81d4035ae6db2254f79308e334f'
+          }
+        ]
+    });
 
 
 
   }, 100000);
+it('generic scheme proposal append ', async () => {
+  const accounts = web3.eth.accounts.wallet;
+
+  const genericScheme = new web3.eth.Contract(
+    GenericScheme.abi,
+    addresses.GenericScheme,
+    opts,
+  );
+  const genesisProtocol = new web3.eth.Contract(
+    GenesisProtocol.abi,
+    addresses.GenesisProtocol,
+    opts,
+  );
+
+  const actionMock = new web3.eth.Contract(
+    ActionMock.abi,
+    addresses.ActionMock,
+    opts,
+  );
+
+  let proposalIPFSData = {
+    description: 'Setting new Icon',
+    title: 'New Icon',
+    url: 'http://swift.org/modest',
+    key: 'Icon',
+    value: 'https://en.wikipedia.org/wiki/River#/media/File:Melting_Toe_of_Athabasca_Glacier.jpg',
+  };
+
+  let proposalDescription = proposalIPFSData.description;
+  let proposalTitle = proposalIPFSData.title;
+  let proposalUrl = proposalIPFSData.url;
+
+  let descHash = await writeProposalIPFS(proposalIPFSData);
+  let callData = await actionMock.methods.test2(addresses.TestAvatar).encodeABI();
+
+  async function propose() {
+    const prop = genericScheme.methods.proposeCall(addresses.TestAvatar, callData, 0, descHash);
+    const proposalId = await prop.call();
+    const { blockNumber } = await prop.send();
+    const { timestamp } = await web3.eth.getBlock(blockNumber);
+    return { proposalId, timestamp };
+  }
+
+  const [PASS, FAIL] = [1, 2];
+  async function vote({ proposalId, outcome, voter, amount = 0 }) {
+    const { blockNumber } = await genesisProtocol.methods
+      .vote(proposalId, outcome, amount, voter)
+      .send({ from: voter });
+    const { timestamp } = await web3.eth.getBlock(blockNumber);
+    return timestamp;
+  }
+
+  const { proposalId: p1, timestamp: p1Creation } = await propose();
+  //console.log(p1)
+  const getProposal = `{
+    proposal(id: "${p1}") {
+        id
+        descriptionHash
+        stage
+        createdAt
+        executedAt
+        proposer
+        votingMachine
+
+        genericScheme {
+          id
+          dao {
+             id
+          }
+          contractToCall
+          callData
+          value
+          executed
+          returnValue
+        }
+        scheme {
+          uGenericSchemeParams {
+            contractToCall
+          }
+        }
+    }
+}`;
+
+
+  let proposal = (await sendQuery(getProposal)).proposal;
+  expect(proposal).toMatchObject({
+    id: p1,
+    descriptionHash: descHash,
+    stage: 'Queued',
+    createdAt: p1Creation.toString(),
+    executedAt: null,
+    proposer: web3.eth.defaultAccount.toLowerCase(),
+    votingMachine: genesisProtocol.options.address.toLowerCase(),
+
+    genericScheme: {
+      id: p1,
+      dao: {
+        id: addresses.TestAvatar.toLowerCase(),
+      },
+      contractToCall: actionMock.options.address.toLowerCase(),
+      callData,
+      value: '0',
+      executed: false,
+      returnValue: null,
+    },
+    scheme: {
+      uGenericSchemeParams: {
+        contractToCall: actionMock.options.address.toLowerCase(),
+      },
+    },
+  });
+
+  await vote({
+    proposalId: p1,
+    outcome: PASS,
+    voter: accounts[0].address,
+  });
+
+  await vote({
+    proposalId: p1,
+    outcome: PASS,
+    voter: accounts[1].address,
+  });
+
+  await vote({
+    proposalId: p1,
+    outcome: PASS,
+    voter: accounts[2].address,
+  });
+
+  let executedAt = await vote({
+    proposalId: p1,
+    outcome: PASS,
+    voter: accounts[3].address,
+  });
+
+  const executedIsIndexed = async () => {
+    return (await sendQuery(getProposal)).proposal.executedAt != null;
+  };
+
+  await waitUntilTrue(executedIsIndexed);
+
+  proposal = (await sendQuery(getProposal)).proposal;
+  expect(proposal).toMatchObject({
+    id: p1,
+    descriptionHash: descHash,
+    stage: 'Executed',
+    createdAt: p1Creation.toString(),
+    executedAt: executedAt + '',
+    proposer: web3.eth.defaultAccount.toLowerCase(),
+    votingMachine: genesisProtocol.options.address.toLowerCase(),
+
+    genericScheme: {
+      id: p1,
+      dao: {
+        id: addresses.TestAvatar.toLowerCase(),
+      },
+      contractToCall: actionMock.options.address.toLowerCase(),
+      callData,
+      value: '0',
+      executed: true,
+      returnValue: '0x0000000000000000000000000000000000000000000000000000000000000001',
+    },
+  });
+
+  const metaq = `{
+    signals{
+      id
+      data
+    }
+  }`
+
+  const metadata = await sendQuery(metaq, 5000);
+
+  expect(metadata).toMatchObject({
+     signals:[
+        {
+          data:
+            '{"Header":"https://de.wikipedia.org/wiki/Wald#/media/Datei:Laurisilva_en_el_Cubo_de_la_Galga.jpg","Icon":"https://en.wikipedia.org/wiki/River#/media/File:Melting_Toe_of_Athabasca_Glacier.jpg"}',
+          id:
+            '0xe7a2c59e134ee81d4035ae6db2254f79308e334f'
+        }
+      ]
+  });
+
+
+
+}, 100000);
+
 });

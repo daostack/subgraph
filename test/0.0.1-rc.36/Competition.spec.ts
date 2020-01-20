41912e18eb9533d897f1d80aed93d1696bcbbf66
Ben <ben.kaufman10@gmail.com>
2020-01-20 10:45:17 +0200

    Update Competition.spec.ts

--- test/0.0.1-rc.36/Competition.spec.ts
+++ test/0.0.1-rc.36/Competition.spec.ts
@@ -430,7 +430,7 @@ describe('Competition', () => {
             }
         }`;
 
-        expect((await sendQuery(proposalVotesQuery)).competitionProposal).toEqual({
+        expect((await sendQuery(proposalVotesQuery))).toContainEqual({
             votes: [
                 { suggestion: { suggestionId: suggestionId1.toString() }, createdAt: timestampVote1.toString() },
                 { suggestion: { suggestionId: suggestionId2.toString() }, createdAt: timestampVote2.toString() },
@@ -439,10 +439,10 @@ describe('Competition', () => {
             snapshotBlock: blockNumberVote1.toString(),
             winningSuggestions: [
                 {
-                    suggestionId: suggestionId1.toString(),
+                    suggestionId: suggestionId2.toString(),
                 },
                 {
-                    suggestionId: suggestionId2.toString(),
+                    suggestionId: suggestionId1.toString(),
                 },
             ],
         });

357cf13dac786359a89f21179b46cb9933cf344a
benk10 <ben.kaufman10@gmail.com>
2021-01-11 08:39:39 +0200

    Update ContinuousLocking4Reputation.spec.ts

--- test/0.0.1-rc.55/ContinuousLocking4Reputation.spec.ts
+++ test/0.0.1-rc.55/ContinuousLocking4Reputation.spec.ts
@@ -57,7 +57,7 @@ describe('ContinuousLocking4Reputation', () => {
       let repRewardConstA = 85000;
       let repRewardConstB = 900;
       let periodsCap = 100;
-      let agreementHash = '0x0000000000000000000000000000000000000001';
+      let agreementHash = '0x00000000000000000000000000000000000000000000000000000000000001';
       const continuousLocking4ReputationCreateTx =
       continuousLocking4ReputationFactory.methods.createCL4R(
         addresses.Avatar,
@@ -196,7 +196,7 @@ describe('ContinuousLocking4Reputation', () => {
                 repRewardConstA: (await continuousLocking4Reputation.methods.repRewardConstA().call()),
                 repRewardConstB: (await continuousLocking4Reputation.methods.repRewardConstB().call()),
                 batchesIndexCap: periodsCap.toString(),
-                agreementHash: padZeros(agreementHash, 64),
+                agreementHash,
             },
           });
 

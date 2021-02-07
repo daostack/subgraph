75c948dc0834351a167151c33bf18dd726f3fbad
benk10 <ben.kaufman10@gmail.com>
2021-02-07 11:48:45 +0200

    Arc v56

--- test/0.0.1-rc.56/util.ts
+++ test/0.0.1-rc.56/util.ts
@@ -54,7 +54,7 @@ export async function getWeb3() {
 
 export function getContractAddresses() {
   const addresses = require(`@daostack/migration/migration.json`);
-  let arcVersion = '0.0.1-rc.55';
+  let arcVersion = '0.0.1-rc.56';
   return {
     ...addresses.private.test[arcVersion],
     ...addresses.private.dao[arcVersion],
@@ -71,12 +71,12 @@ export function getContractAddresses() {
 }
 
 export function getArcVersion() {
-  return '0.0.1-rc.55';
+  return '0.0.1-rc.56';
 }
 
 export function getOrgName() {
   return require(`@daostack/migration/migration.json`).private.dao[
-    '0.0.1-rc.55'
+    '0.0.1-rc.56'
   ].name;
 }
 
